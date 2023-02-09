daa62e40dbf8bf76e32c727b47535d1c8a2a5dfd
mikhoiuo <demibabeslay@gmail.com>
2023-02-09 08:42:13 -0500

    shasum ... again

--- hackerman.rb
+++ hackerman.rb
@@ -2,7 +2,7 @@ class Hackerman < Formula
     desc "The ultimate hacker jargon generator"
     homepage "https://github.com/mikhoiuo/homebrew-hackerman"
     url "https://github.com/mikhoiuo/homebrew-hackerman/blob/d89ccbb2156f6ab2dc08eae8af737d05f6c6e0fb/hackerman-0.1.0.tar.gz"
-    sha256 "6e3bef8631489136b75a0d7c78a5f944b412d500dce1d0f034b8c078dae3d175"
+    sha256 "6822c52cfb5b9f7b3496e9b0426b7b8a18aa7f87b358f231ddec39986dab7673"
   
     def install
       bin.install "hackerman"
