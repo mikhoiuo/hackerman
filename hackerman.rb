44e4e1de5f03e8ddabd4e8042940f357114e48d0
mikhoiuo <demibabeslay@gmail.com>
2023-02-09 09:31:25 -0500

    YES

--- hackerman.rb
+++ hackerman.rb
@@ -2,7 +2,7 @@ class Hackerman < Formula
     desc "The ultimate hacker jargon generator"
     homepage "https://github.com/mikhoiuo/homebrew-hackerman"
     url "https://github.com/mikhoiuo/homebrew-hackerman/archive/refs/tags/real2.tar.gz"
-    sha256 "6822c52cfb5b9f7b3496e9b0426b7b8a18aa7f87b358f231ddec39986dab7673"
+    sha256 "a8e4cb9090fd4f2084e7acfe759f815ca2637179a16bc792bacdd1db623a7194"
     depends_on "pv"
   
     def install
