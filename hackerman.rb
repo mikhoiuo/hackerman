84c50f3d991502c2fc00d5b69df19e2dee05aff9
mikhoiuo <demibabeslay@gmail.com>
2023-02-09 09:47:13 -0500

    new desc + readme

--- hackerman.rb
+++ hackerman.rb
@@ -1,5 +1,5 @@
 class Hackerman < Formula
-    desc "The ultimate hacker jargon generator"
+    desc "live out your movie hacker man fantasies with no talent or skill! yay!"
     homepage "https://github.com/mikhoiuo/homebrew-hackerman"
     url "https://github.com/mikhoiuo/homebrew-hackerman/archive/refs/tags/real2.tar.gz"
     sha256 "a8e4cb9090fd4f2084e7acfe759f815ca2637179a16bc792bacdd1db623a7194"
@@ -13,4 +13,3 @@ class Hackerman < Formula
       system "#{bin}/hackerman"
     end
   end
-  
