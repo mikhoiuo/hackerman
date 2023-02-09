7864aa40c0c6bbae4657be9b6b456819bbcb48f4
mikhoiuo <demibabeslay@gmail.com>
2023-02-09 09:29:03 -0500

    yes

--- hackerman.rb
+++ hackerman.rb
@@ -3,6 +3,7 @@ class Hackerman < Formula
     homepage "https://github.com/mikhoiuo/homebrew-hackerman"
     url "https://github.com/mikhoiuo/homebrew-hackerman/archive/refs/tags/real2.tar.gz"
     sha256 "6822c52cfb5b9f7b3496e9b0426b7b8a18aa7f87b358f231ddec39986dab7673"
+    depends_on "pv"
   
     def install
       bin.install "hackerman"
